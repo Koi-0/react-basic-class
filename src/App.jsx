cb785b97389ec46f63d722c8752cfc82f265d552
Koi-0 <dndud5182@gmail.com>
2025-01-23 14:47:01 +0900

    Todo 리스트  : Create 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import TodoList from "./components/TodoList";
 
 const App = () => {
+    // const [상태, 상태변경함수] = useState(초기값)
+
     return (
         <main className="main-center">
             <TodoList />
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
+
+const SAMPLE_TODOS = [
+    { id: 1, text: "Buy milk" },
+    { id: 2, text: "Clean the house" },
+    { id: 3, text: "Go for a run" },
+    { id: 4, text: "Finish homework" },
+    { id: 5, text: "Call mom" },
+    { id: 6, text: "Buy groceries" },
+    { id: 7, text: "Walk the dog" },
+    { id: 8, text: "Read a book" },
+    { id: 9, text: "Do laundry" },
+    { id: 10, text: "Write code" },
+];
+
 const TodoList = () => {
-    const todos = [
-        { id: 1, text: "Buy milk" },
-        { id: 2, text: "Clean the house" },
-        { id: 3, text: "Go for a run" },
-        { id: 4, text: "Finish homework" },
-        { id: 5, text: "Call mom" },
-        { id: 6, text: "Buy groceries" },
-        { id: 7, text: "Walk the dog" },
-        { id: 8, text: "Read a book" },
-        { id: 9, text: "Do laundry" },
-        { id: 10, text: "Write code" },
-    ];
+    const [todos, setTodos] = useState(SAMPLE_TODOS);
+
+    const [newTodo, setNewTodo] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // <2>
+
+        if (!newTodo.trim()) {
+            return; // <3>
+        }
+
+        setTodos([{ id: crypto.randomUUID(), text: newTodo }, ...todos]); // <4>
+        setNewTodo(""); // <5>
+    };
+
+    const handleInputChange = (e) => {
+        setNewTodo(e.target.value);
+    };
 
     return (
-        <ul>
-            {todos.map((todo) => (
-                <li key={todo.id}>{todo.text}</li>
-            ))}
-        </ul>
+        <div>
+            {/* <1> */}
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={newTodo} onChange={handleInputChange} placeholder="Enter a new todo" />
+                <button type="submit">Add Todo</button>
+            </form>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.text}</li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
