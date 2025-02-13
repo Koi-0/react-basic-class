1f864db943a3aab245c2bd487c1ed8df1d32bb6f
Koi-0 <dndud5182@gmail.com>
2025-02-14 02:45:37 +0900

    Feat : TodoProvider 채우기 40% 완료

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import TodoItem from "./TodoItem";
+import { useContext } from "react";
+import { TodoContext } from "../../context/TodoContext";
 
-const TodoList = ({ todos, toggleTodoCompleted, deleteTodo }) => {
+const TodoList = () => {
+    const { todos } = useContext(TodoContext);
+
     return (
         <TodoListSection>
             <TodoListHeader>Tasks</TodoListHeader>
             <TodoListContent>
                 {todos.map(({ id, text, completed }) => (
-                    <TodoItem key={id} completed={completed} text={text} toggleTodoCompleted={toggleTodoCompleted} deleteTodo={deleteTodo} id={id} />
+                    <TodoItem key={id} completed={completed} text={text} id={id} />
                 ))}
             </TodoListContent>
         </TodoListSection>
--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,9 +1,3 @@
 import { createContext } from "react";
 
 export const TodoContext = createContext();
-
-const TodoProvider = () => {
-    return <div>TodoContext</div>;
-};
-
-export default TodoProvider;
