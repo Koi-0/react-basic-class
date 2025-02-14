a2aaf942c012aa07092015676aa713a9491f9c45
Koi-0 <dndud5182@gmail.com>
2025-02-14 10:36:25 +0900

    Feat : TodoItem 컴포넌트에서 할 일 텍스트를 링크로 변경하여 상세 페이지로 이동 가능하도록 수정

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import styled from "styled-components";
 import { TodoContext } from "../../context/TodoContext";
+import { Link } from "react-router";
 
 const TodoItem = ({ completed, text, id }) => {
     const { toggleTodoCompleted, deleteTodo } = useContext(TodoContext);
 
     return (
         <TodoItemWrapper>
-            <TodoItemText $completed={completed}>{text}</TodoItemText>
+            <TodoItemLink to={`/todos/${id}`} $completed={completed}>
+                {text}
+            </TodoItemLink>
             <TodoItemActions>
                 <ActionButton onClick={() => toggleTodoCompleted(id)} $bgColor={completed ? "#242424" : "#582be6"}>
                     {completed ? "취소하기" : "완료하기"}
@@ -33,8 +36,11 @@ const TodoItemWrapper = styled.li`
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 `;
 
-const TodoItemText = styled.p`
+const TodoItemLink = styled(Link)`
     text-decoration: ${({ $completed }) => ($completed ? "line-through" : "none")};
+    &:hover {
+        text-decoration: underline;
+    }
 `;
 
 const TodoItemActions = styled.div`
