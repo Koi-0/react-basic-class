d1b86d338761b22fca62913b0df0f52e73370f04
Young <dndud5182@gmail.com>
2025-03-07 01:12:11 +0900

    Feat : useToast 훅에서 Toast 자동 제거 기능 추가

--- src/hooks/useToast.js
+++ src/hooks/useToast.js
@@ -2,26 +2,22 @@ import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 
 export const useToast = create(
-  immer((set) => ({
-    toasts: [
-      {
-        id: 1,
-        message: "Hello, World!",
-      },
-      {
-        id: 2,
-        message: "This is a toast message",
-      },
-    ],
+  immer((set, get) => ({
+    toasts: [],
 
     addToast: (message) => {
       set((prevState) => {
         const id = crypto.randomUUID();
+        const { removeToast } = get();
 
         prevState.toasts.push({
           id,
           message,
         });
+
+        setTimeout(() => {
+          removeToast(id);
+        }, 3000);
       });
     },
 
