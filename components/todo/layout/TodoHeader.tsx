817c269aa68894ec67cad92bbff62c8c527945a6
Young <dndud5182@gmail.com>
2025-03-16 19:59:54 +0900

    Feat : TodoHeader 컴포넌트에 홈으로 돌아가기 링크 추가

--- components/todo/layout/TodoHeader.tsx
+++ components/todo/layout/TodoHeader.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 const TodoHeader = () => {
   return (
     <header className="mb-4">
-      <div className="container mx-auto p-2">
-        <h1 className="text-4xl font-bold">Todo List</h1>
+      <div className="container mx-auto flex p-2">
+        <Link href={"/"}>
+          <h1 className="text-4xl font-bold">Todo List</h1>
+        </Link>
       </div>
     </header>
   );
