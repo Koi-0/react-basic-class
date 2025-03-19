0e28664e7c84e6b7c145ac9ed61d33ac0fb7ce4e
Young <dndud5182@gmail.com>
2025-03-19 14:34:07 +0900

    Feat : React Query를 사용하여 TodoList 컴포넌트의 데이터 fetching 방식 변경

--- api/todo-api.ts
+++ api/todo-api.ts
@@ -1,7 +1,4 @@
-"use server";
-
 import { Todo } from "@/types/todo.type";
-import { revalidateTag } from "next/cache";
 
 const BASE_URL = "http://localhost:3000/todos";
 
@@ -40,8 +37,6 @@ export const createTodo = async (text: Todo["text"]) => {
 
   const data: Todo[] = await response.json();
 
-  revalidateTag("todos");
-
   return data;
 };
 
@@ -51,8 +46,6 @@ export const deleteTodo = async (id: Todo["id"]) => {
   });
 
   const data: Todo[] = await response.json();
-
-  revalidateTag("todos");
 
   return data;
 };
@@ -70,8 +63,6 @@ export const toggleTodoCompleted = async (
   });
 
   const data: Todo[] = await response.json();
-
-  revalidateTag("todos");
 
   return data;
 };
--- components/todo/TodoList.tsx
+++ components/todo/TodoList.tsx
@@ -1,8 +1,12 @@
-import { getTodos } from "@/api/todo-api";
+"use client";
+
+import { useTodosQuery } from "@/query/useTodoQuery";
 import TodoItem from "./TodoItem";
 
-const TodoList = async () => {
-  const todos = await getTodos();
+const TodoList = () => {
+  const { data: todos } = useTodosQuery(); // 클라이언트 사이드 렌더링
+
+  if (!todos) return <div>Loading...</div>;
 
   return (
     <ul className="space-y-2">
