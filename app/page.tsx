e78bdf266688fe5ab7c8ef6a27ba39e210f3d99f
Young <dndud5182@gmail.com>
2025-03-20 00:46:42 +0900

    Feat : prefetch를 이용하여 SSR 구현 및 SEO 개선

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
+import { getTodos } from "@/api/todo-api";
 import TodoForm from "@/components/todo/TodoForm";
 import TodoList from "@/components/todo/TodoList";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+
   return (
-    <section>
-      <div className="container mx-auto space-y-8 p-2">
-        <TodoForm />
-        <TodoList />
-      </div>
-    </section>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <section>
+        <div className="container mx-auto space-y-8 p-2">
+          <TodoForm />
+          <TodoList />
+        </div>
+      </section>
+    </HydrationBoundary>
   );
 };
 
