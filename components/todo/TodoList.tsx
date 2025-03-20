81e954c367bf77ebb87f7fd27d44821a0c274fde
Young <dndud5182@gmail.com>
2025-03-21 00:27:35 +0900

    Feat : Todo API 및 쿼리에서 필터링 기능 추가

--- components/todo/TodoList.tsx
+++ components/todo/TodoList.tsx
@@ -2,9 +2,11 @@
 
 import { useTodosQuery } from "@/query/useTodoQuery";
 import TodoItem from "./TodoItem";
+import { useTodoFilterStore } from "@/store/useTodoFilterStore";
 
 const TodoList = () => {
-  const { data: todos } = useTodosQuery(); // 클라이언트 사이드 렌더링
+  const { filter } = useTodoFilterStore();
+  const { data: todos } = useTodosQuery(filter); // 클라이언트 사이드 렌더링
 
   if (!todos) return <div>Loading...</div>;
 
